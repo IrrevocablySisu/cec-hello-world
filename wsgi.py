9dd496cbb9c9373f39dc008e77dbb7874b9df200
A.Pinçon <dev.pincon@gmail.com>
2018-02-26 07:24:56 +0200

    Fonctions pour ecrire log et afficher sur application

--- wsgi.py
+++ wsgi.py
@@ -1,13 +1,27 @@
 import socket
+import datetime
 from flask import Flask
 
 application = Flask(__name__)
 
+
 @application.route("/")
 def hello():
-    
-    return "Salut Monde! Greetings from "+socket.gethostname()+"\n"
+
+    writeInLog("log")
+    string = logToString("log")
+    return "Salut Monde! Greetings from "+socket.gethostname()+"\n"+string+"\n"
 
 
 if __name__ == "__main__":
     application.run()
+
+
+def writeInLog(logfile):
+    file = open(logfile, "w")
+    file.write("Host: "+socket.gethostname()+"  Timestamp: {:%Y-%m-%d %H:%M:%S}".format(datetime.datetime.now())+"\n")
+    file.close()
+
+def logToString(logfile):
+    file = open(logfile, "r")
+    return file.read()
