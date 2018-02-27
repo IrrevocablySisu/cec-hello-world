a35eec36342fe0c9bcfef8b2ff70a95239193d55
A.Pinçon <dev.pincon@gmail.com>
2018-02-27 08:55:15 +0200

    Catch error

--- wsgi.py
+++ wsgi.py
@@ -8,8 +8,15 @@ application = Flask(__name__)
 @application.route("/")
 def hello():
 
-    write_in_log("/mnt/log/logs/log.txt")
-    log = log_to_string("log.txt")
+    log = ""
+
+    try:
+        write_in_log("/mnt/log/logs/log.txt")
+        log = log_to_string("/mnt/log/logs/log.txt")
+
+    except Exception as e:
+        print(e)
+
     hello_world = "Hello World ! <br><br>"
     final_string = hello_world+log
 
