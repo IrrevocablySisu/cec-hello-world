5bc12871d3b8f72fc50112e1018d2019d53c8bd5
A.Pinçon <dev.pincon@gmail.com>
2018-02-27 06:36:15 +0200

    Respect de la syntaxe python

--- wsgi.py
+++ wsgi.py
@@ -8,24 +8,27 @@ application = Flask(__name__)
 @application.route("/")
 def hello():
 
-    writeInLog("log.txt")
-    string = logToString("log.txt")
-    finalString = "Salut Monde! Greetings from "+socket.gethostname()+"\n"+string+"\n"
-    return finalString
+    write_in_log("log.txt")
+    log = log_to_string("log.txt")
+    hello_world = "Hello World!\n"
+    final_string = hello_world+log
+
+    return final_string
 
 
 if __name__ == "__main__":
     application.run()
 
 
-def writeInLog(logfile):
+def write_in_log(logfile):
     file = open(logfile, "w")
     file.write("Host: "+socket.gethostname()+"  Timestamp: {:%Y-%m-%d %H:%M:%S}".format(datetime.datetime.now())+"\n")
     file.close()
 
 
-def logToString(logfile):
+def log_to_string(logfile):
     file = open(logfile, "r")
     string = file.read()
     file.close()
+
     return string
