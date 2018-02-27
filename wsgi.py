13440b83d568fa0502feb44faac0ae153fda8b74
A.Pinçon <dev.pincon@gmail.com>
2018-02-27 09:28:09 +0200

    Suppression de l'ancien log

--- wsgi.py
+++ wsgi.py
@@ -26,7 +26,7 @@ if __name__ == "__main__":
 
 
 def write_in_log(logfile):
-    file = open(logfile, "a")
+    file = open(logfile, "w")
     file.write("Host: "+socket.gethostname()+"  Timestamp: {:%Y-%m-%d %H:%M:%S}".format(datetime.datetime.now())+"<br>")
     file.close()
 
